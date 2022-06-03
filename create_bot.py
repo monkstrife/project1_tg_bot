6736fb5eec6aee431211441a63054b0dd7384465
monkstrife <drobilko2002@mail.ru>
2022-06-03 19:22:34 +0300

    token off

--- create_bot.py
+++ create_bot.py
@@ -4,7 +4,7 @@ import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
-token = os.getenv('TOKEN')
-bot = Bot(token=token)
+#token = os.getenv('TOKEN')
+bot = Bot(token='5318034841:AAE8LwugFT2Ne-cRpFH6QdJ0mD398M2WiYU')
 dp = Dispatcher(bot, storage=storage)
 #'5318034841:AAE8LwugFT2Ne-cRpFH6QdJ0mD398M2WiYU'
